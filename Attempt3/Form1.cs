83fe5a0df478d9eccc71cab3a845537d7229a730
Lemoriz <alimuahmed.ramil@gmail.com>
2017-09-09 02:33:20 +0500

    st comm

---
+++ Attempt3/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Attempt3
+{
+    public partial class MainScreen : Form
+    {
+        public MainScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void MainScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreateProjectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpenProjectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveProjectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CloseProjectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FileStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreateProjectToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpenProjectToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveProjectToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CloseProjectToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExitToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void EditToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void СalculationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PerformToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ConstructAGraphToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AboutTheProgramToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
